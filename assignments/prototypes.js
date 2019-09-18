c630fe05af25502cadee59db9026fcaa457d544e
Joe O <j0liverko@gmail.com>
2019-09-18 15:47:48 -0500

    Finished this.js

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -15,6 +15,14 @@
   * dimensions (These represent the character's size in the video game)
   * destroy() // prototype method that returns: `${this.name} was removed from the game.`
 */
+   function GameObject(properties){
+      this.createdAt = properties.createdAt,
+      this.name = properties.name,
+      this.dimensions = properties.dimensions
+  }
+  gameObject.prototype.destroy = function(){
+    return  `${this.name} was removed from the game`;
+  }
 
 /*
   === CharacterStats ===
@@ -22,6 +30,13 @@
   * takeDamage() // prototype method -> returns the string '<object name> took damage.'
   * should inherit destroy() from GameObject's prototype
 */
+function CharacterStats(properties){
+  this.healthPoints = properties.healthPoints,
+  
+}
+characterStats.prototype.takeDamage = function(){
+return  `${this.name} took damage`;
+}
 
 /*
   === Humanoid (Having an appearance or character resembling that of a human.) ===
@@ -32,7 +47,14 @@
   * should inherit destroy() from GameObject through CharacterStats
   * should inherit takeDamage() from CharacterStats
 */
- 
+function Humanoid(properties){
+  this.team = properties.team,
+  this.weapons = properties.weapons,
+  this.language = properties.language
+}
+Humanoid.prototype.greet = function(){
+  return  `${this.name} offers a greeting in ${this.language}`;
+  }
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,57 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window-binding: default action for this 
+* 2. Implict-binding: automatic binding most common
+* 3. Explict-binding: forcing calling a fucntion changing this
+* 4. New-Binding: for oop way of creating obj template
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+// // Principle 1
+// // 'use strict';
+//      function ghost(){
+//       console.log(this.boo);
+//     }
+//      ghost();
+//     const boo = '👻';
 
-// code example for Window Binding
+// // Principle 2
+//     let myDog = {
+//         name: 'Doggy',
+//         pronoun: 'it',
+//         sound: 'woof',
+//         bark: function(){
+//             console.log(this.sound)
+//         }
+//     }
+//     myDog.bark();
+// // code example for Implicit Binding
 
-// Principle 2
+// // Principle 3
 
-// code example for Implicit Binding
-
-// Principle 3
-
-// code example for New Binding
+// function Person(properties){
+//     this.newFirstName = properties.firstName,
+//     this.newLastName = properties.lastName,
+//     this.newGender = properties.gender,
+//     this.newEmail = properties.email
+// }
+// const Joe = new Person({
+//     firstName: 'Joe',
+//     lastName: 'Oliver',
+//     gender: 'M',
+//     email: 'yomamma@gmail.com'
+// })
 
 // Principle 4
+function greet() {
+	console.log(this.name);
+}
 
+var person = {
+	name:'JOJO'
+}
+
+greet.call(person);
 // code example for Explicit Binding
